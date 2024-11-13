7fba36ad5a5cbd3c4e50829e86ebe352d2a130cd
juliengp <julien.garciaperez@gmail.com>
2024-11-13 15:13:01 +0100

    Images des collections ok

--- app/src/main/java/com/example/premiereappli/HorreurScreen.kt
+++ app/src/main/java/com/example/premiereappli/HorreurScreen.kt
@@ -1,12 +1,22 @@
 package com.example.premiereappli
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun HorreurScreen(viewModel: MainViewModel) {
@@ -16,15 +26,41 @@ fun HorreurScreen(viewModel: MainViewModel) {
         viewModel.getCollection("horror")
     }
 
-    LazyColumn{
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize().padding(8.dp)
+    ) {
         collectionResults?.results?.let { collections ->
             items(collections) { collection ->
-                Text(text = collection.name)
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxSize(),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        if (collection.poster_path != null) {
+                            AsyncImage(
+                                model = "https://image.tmdb.org/t/p/w500${collection.poster_path}",
+                                contentDescription = collection.name,
+                                modifier = Modifier
+                                    .padding(bottom = 8.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                        }
+                        Text(
+                            text = collection.name?: "Titre de collection inconnu",
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+                }
             }
-        }
-            ?:item {
-                Text("No collections found")
+        } ?: item {
+            Text("No collections found")
         }
     }
-
 }
